3c12741000aba15a7c667040dee5eb7f6d90640f
alpha-hexor <itsalexwilliams69@gmail.com>
2024-05-23 23:49:31 +0530

    Web rtc added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for, send_from_directory, render_template_string
+from flask import Flask, request, jsonify, render_template, redirect, url_for, send_from_directory, render_template_string,session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import os
@@ -10,6 +10,8 @@ from flask_bcrypt import Bcrypt
 from Crypto.PublicKey import RSA
 import uuid
 from Crypto.Cipher import PKCS1_OAEP
+from engineio.payload import Payload
+Payload.max_decode_packets = 200
 
 app = Flask(__name__)
 
@@ -34,6 +36,12 @@ app.static_folder = 'static'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+_users_in_room = {} # stores room wise user list
+_room_of_sid = {} # stores room joined by an used
+_name_of_sid = {} # stores display name of users
+
+
 
 # Define the User model
 class ChatMessage(db.Model):
@@ -329,6 +337,32 @@ def change_avatar():
 
     return redirect(url_for('settng_page'))
 
+@app.route("/video_room", methods=["GET", "POST"])
+def video_room():
+    if request.method == "POST":
+        room_id = request.form['room_id']
+        return redirect(url_for("entry_checkpoint", room_id=room_id))
+
+    return render_template("video_room.html")
+
+@app.route("/room/<string:room_id>/")
+def enter_room(room_id):
+    if room_id not in session:
+        return redirect(url_for("entry_checkpoint", room_id=room_id))
+    return render_template("chatroom.html", room_id=room_id, display_name=session[room_id]["name"], mute_audio=session[room_id]["mute_audio"], mute_video=session[room_id]["mute_video"])
+
+@app.route("/room/<string:room_id>/checkpoint/", methods=["GET", "POST"])
+def entry_checkpoint(room_id):
+    if request.method == "POST":
+        display_name = request.form['display_name']
+        mute_audio = request.form['mute_audio']
+        mute_video = request.form['mute_video']
+        session[room_id] = {"name": display_name, "mute_audio":mute_audio, "mute_video":mute_video}
+        return redirect(url_for("enter_room", room_id=room_id))
+
+    return render_template("chatroom_checkpoint.html", room_id=room_id)
+
+
 @app.route("/logout", methods=["GET"])
 @login_required
 def logout():
@@ -472,7 +506,69 @@ def handle_delete_message(data):
         # Emit an event to inform all clients in the room to delete the message
         socketio.emit('message_deleted', {'message_id': message_id, 'room_id': room_id}, room=room_id)
 
+# web rtc stuff
+@socketio.on("connect")
+def on_connect():
+    sid = request.sid
+    print("New socket connected ", sid)
+    
 
+@socketio.on("join-room")
+def on_join_room(data):
+    sid = request.sid
+    room_id = data["room_id"]
+    display_name = session[room_id]["name"]
+    
+    # register sid to the room
+    join_room(room_id)
+    _room_of_sid[sid] = room_id
+    _name_of_sid[sid] = display_name
+    
+    # broadcast to others in the room
+    print("[{}] New member joined: {}<{}>".format(room_id, display_name, sid))
+    emit("user-connect", {"sid": sid, "name": display_name}, broadcast=True, include_self=False, room=room_id)
+    
+    # add to user list maintained on server
+    if room_id not in _users_in_room:
+        _users_in_room[room_id] = [sid]
+        emit("user-list", {"my_id": sid}) # send own id only
+    else:
+        usrlist = {u_id:_name_of_sid[u_id] for u_id in _users_in_room[room_id]}
+        emit("user-list", {"list": usrlist, "my_id": sid}) # send list of existing users to the new member
+        _users_in_room[room_id].append(sid) # add new member to user list maintained on server
+
+    print("\nusers: ", _users_in_room, "\n")
+
+
+@socketio.on("disconnect")
+def on_disconnect():
+    sid = request.sid
+    room_id = _room_of_sid[sid]
+    display_name = _name_of_sid[sid]
+
+    print("[{}] Member left: {}<{}>".format(room_id, display_name, sid))
+    emit("user-disconnect", {"sid": sid}, broadcast=True, include_self=False, room=room_id)
+
+    _users_in_room[room_id].remove(sid)
+    if len(_users_in_room[room_id]) == 0:
+        _users_in_room.pop(room_id)
+
+    _room_of_sid.pop(sid)
+    _name_of_sid.pop(sid)
+
+    print("\nusers: ", _users_in_room, "\n")
+
+
+@socketio.on("data")
+def on_data(data):
+    sender_sid = data['sender_id']
+    target_sid = data['target_id']
+    if sender_sid != request.sid:
+        print("[Not supposed to happen!] request.sid and sender_id don't match!!!")
+
+    if data["type"] != "new-ice-candidate":
+        print('{} message from {} to {}'.format(data["type"], sender_sid, target_sid))
+    socketio.emit('data', data, room=target_sid)
 
 if __name__ == '__main__':
     create_db()
